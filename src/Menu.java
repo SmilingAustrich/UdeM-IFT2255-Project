b4c435fdfe746d5416f449ad0a2aad66574deacc
tarek <tarik.hireche@umontreal.ca>
2024-10-07 14:46:08 -0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml
    #	src/Intervenant.java
    #	src/Menus.java
    #	src/Resident.java

--- src/Menu.java
+++ src/Menu.java
@@ -79,7 +79,6 @@ public class Menu {
                 break;
             }
 
-
         }
 
         if (loggedInResident) {
@@ -224,6 +223,78 @@ public class Menu {
         AppSimulation.simulateLoading();
 
         intervenantLogInMenu();
+    }
+
+    public void intervenantMainMenu(User user) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("\n*******************************\n" +
+                "Vous êtes actuellement sur le menu des intervenants de l'application Ma Ville.\n" +
+                "\n Veuillez choisir une option dans la liste de souhait suivante \n" +
+                "\t 1. Consulter la liste des requêtes de travail.\n" +
+                "\t 2. Soumettre un nouveau projet de travaux.\n" +
+                "\t 3. Mettre à jour les informations d'un chantier.\n" +
+                " Insérer la lettre qui correspond à votre choix : ");
+        int choice = in.nextInt();
+        in.nextLine();
+        System.out.println("*******************************");
+        switch (choice) {
+            case 1 :
+                System.out.println("Voulez vous consuler la liste des requêtes de travaux et soumettre " +
+                        "votre candidature ?");
+                break;
+            case 2 :
+                System.out.println("Fournissez les informations suivantes (titre du projet, description du projet," +
+                        "types de travaux, quartiers affectés, rue affectées, date de début, date de fin," +
+                        " horaire des travaux) pour soumettre un projet :");
+                break;
+            case 3 :
+                System.out.println("Que voulez-vous mettre à jour ? La description du projet, la date de fin prévu" +
+                        " ou voulez-vous changer le statut du projet ? :");
+                break;
+            default :
+                System.out.println("Choix invalide. Veuillez réessayer.");
+        }
+    }
+
+    public void residentMainMenu(User user){
+        Scanner in = new Scanner(System.in);
+        System.out.print("\n*******************************\n" +
+                "Vous êtes actuellement sur le menu des résidents de l'application Ma Ville.\n" +
+                "\n Veuillez choisir une option dans la liste de souhait suivante : \n" +
+                "\t 1. Consulter les travaux en cours ou à venir.\n" +
+                "\t 2. Rechercher des travaux\n" +
+                "\t 3. Recevoir des notifications personnalisées.\n" +
+                "\t 4. Permettre une planification participative. \n" +
+                "\t 5. Soumettre une requête de travail. \n" +
+                "\t 6. Signaler un problème à la ville. \n" +
+                " Insérer la lettre qui correspond à votre choix : ");
+        int choice = in.nextInt();
+        in.nextLine();
+        System.out.println("*******************************");
+        switch (choice) {
+            case 1 :
+                System.out.println("Voulez-vous consulter les travaux en cours ou sur les 3 prochains mois ?");
+                break;
+            case 2 :
+                System.out.println("Entrer les critères de recherche (par titre, types de travaux ou quartier) :");
+                break;
+            case 3 :
+                System.out.println("Configurez votre rue et votre quartier afin de recevoir des notifications " +
+                        "spécifiques à votre zone :");
+                break;
+            case 4 :
+                System.out.println("Voulez-vous fournir des préférences ou consulter celles des autres ?");
+                break;
+            case 5 :
+                System.out.println("Fournissez les spécifications (titre du travail a réalisé, description détaillée, " +
+                        "date de début espérée...) pour la requête de travail :");
+                break;
+            case 6 :
+                System.out.println("Fournissez les informations suivantes (nom du résident, adresse courriel," +
+                        "adresse de résidence, type de problème, description du problème) pour signaler un problème :");
+            default :
+                System.out.println("Choix invalide. Veuillez réessayer.");
+        }
     }
 
     // Méthode pour démarrer l'application
